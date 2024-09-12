23b917da5c9c0cc95420729b5866db1b2d08f33f
Ryan Sadowski <Ryan.Sadowski@Carvana.com>
2024-09-12 10:27:13 -0700

    💀

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -9,7 +9,24 @@ return {
   config = function()
     local oil = require 'oil'
     oil.setup {
-      -- Oil-specific setup options
+      default_file_explorer = true,
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = 'actions.select_vsplit',
+        ['<C-h>'] = 'actions.select_split',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+      },
     }
 
     -- Set up the custom icons
@@ -32,6 +49,16 @@ return {
     end
 
     -- Add Oil-specific keymaps
-    vim.keymap.set('n', '<leader>ex', '<cmd>Oil<CR>', { desc = 'Open Oil file explorer' })
+    vim.keymap.set('n', '<leader>ex', function()
+      require('oil').open()
+    end, { desc = 'Open Oil file explorer' })
+
+    vim.keymap.set('n', '-', function()
+      if vim.bo.filetype == 'oil' then
+        require('oil.actions').parent.callback()
+      else
+        require('oil').open()
+      end
+    end, { desc = 'Open parent directory or Oil' })
   end,
 }
